5b799a5af8cfaeff7e06b0efc3ee5a86548e0add
KrusidOoO <KrusidOoO@users.noreply.github.com>
2020-03-20 12:46:02 +0100

    End of day commit/push

---
+++ NewCalculator/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NewCalculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #region Numbers
+        private void Num1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Num2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Num3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Num4_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Num5_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Num6_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Num7_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Num8_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Num9_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Num0_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        #endregion
+
+        #region Functions
+
+        #endregion
+
+        #region Clears
+
+        #endregion
+    }
+}
