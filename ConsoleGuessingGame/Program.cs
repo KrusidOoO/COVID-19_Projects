f670df5659b258f2fa3efb3dda1f387641f83825
KrusidOoO <KrusidOoO@users.noreply.github.com>
2020-03-25 12:02:18 +0100

    Commit/pust at 12:00 (25-03-2020)

---
+++ ConsoleGuessingGame/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ConsoleGuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Now it is time for a guessing game, guess a number between 0 and 10");
+            int k = 1;
+            Random rnd = new Random();
+            int o = Convert.ToInt32(rnd.Next(0, 10));
+            int l;
+            while (k <= 3)
+            {
+
+                Console.WriteLine("Make your guess as to which number has been generated");
+                l = Convert.ToInt32(Console.ReadLine());
+                //if(l==l)
+                //{
+                //    Console.WriteLine("This guess is the same as your previous guess, pick a different number");
+                //}
+                if (l > o)
+                {
+                    Console.WriteLine("Your guess is higher than the generated number, try again");
+                    k++;
+                }
+                else if (l < o)
+                {
+                    Console.WriteLine("Your guess is lower than the generated number, try again");
+                    k++;
+                }
+                else if (l == o)
+                {
+                    Console.WriteLine("You are correct!!");
+                    k = 5;
+                    Console.WriteLine("Press any key to exit this fun little program");
+                }
+                if (k > 3)
+                    Console.WriteLine("You ran out of tries, better luck next time!");
+                else if (k == 3)
+                    Console.WriteLine("");
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
