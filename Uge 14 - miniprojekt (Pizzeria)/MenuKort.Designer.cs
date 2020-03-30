c5b3f13a6925ec316e666b37c3dfe70c08781655
KrusidOoO <KrusidOoO@users.noreply.github.com>
2020-03-30 14:31:25 +0200

    Commit/push (30-03-2020)

---
+++ Uge 14 - miniprojekt (Pizzeria)/MenuKort.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace Uge_14___miniprojekt__Pizzeria_
+{
+    partial class MenuKort
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Pizza_Name1 = new System.Windows.Forms.Label();
+            this.Pizza_Name3 = new System.Windows.Forms.Label();
+            this.Pizza_Name2 = new System.Windows.Forms.Label();
+            this.Number_2 = new System.Windows.Forms.Label();
+            this.Number_1 = new System.Windows.Forms.Label();
+            this.Number_3 = new System.Windows.Forms.Label();
+            this.Price_Normal_Header = new System.Windows.Forms.Label();
+            this.Pizza_Description1 = new System.Windows.Forms.Label();
+            this.Pizza_Description2 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Price_Family_Header = new System.Windows.Forms.Label();
+            this.Purchase1 = new System.Windows.Forms.Button();
+            this.Purchase3 = new System.Windows.Forms.Button();
+            this.Purchase2 = new System.Windows.Forms.Button();
+            this.Price_Normal1 = new System.Windows.Forms.CheckBox();
+            this.Price_Normal3 = new System.Windows.Forms.CheckBox();
+            this.Price_Normal2 = new System.Windows.Forms.CheckBox();
+            this.Price_Family3 = new System.Windows.Forms.CheckBox();
+            this.Price_Family2 = new System.Windows.Forms.CheckBox();
+            this.Price_Family1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // Pizza_Name1
+            // 
+            this.Pizza_Name1.AutoSize = true;
+            this.Pizza_Name1.Font = new System.Drawing.Font("Malgun Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Pizza_Name1.Location = new System.Drawing.Point(223, 139);
+            this.Pizza_Name1.Name = "Pizza_Name1";
+            this.Pizza_Name1.Size = new System.Drawing.Size(107, 25);
+            this.Pizza_Name1.TabIndex = 0;
+            this.Pizza_Name1.Text = "Margherita";
+            // 
+            // Pizza_Name3
+            // 
+            this.Pizza_Name3.AutoSize = true;
+            this.Pizza_Name3.Font = new System.Drawing.Font("Malgun Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Pizza_Name3.Location = new System.Drawing.Point(223, 278);
+            this.Pizza_Name3.Name = "Pizza_Name3";
+            this.Pizza_Name3.Size = new System.Drawing.Size(99, 25);
+            this.Pizza_Name3.TabIndex = 1;
+            this.Pizza_Name3.Text = "Prosciutto";
+            // 
+            // Pizza_Name2
+            // 
+            this.Pizza_Name2.AutoSize = true;
+            this.Pizza_Name2.Font = new System.Drawing.Font("Malgun Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Pizza_Name2.Location = new System.Drawing.Point(223, 199);
+            this.Pizza_Name2.Name = "Pizza_Name2";
+            this.Pizza_Name2.Size = new System.Drawing.Size(70, 25);
+            this.Pizza_Name2.TabIndex = 2;
+            this.Pizza_Name2.Text = "Hawaii";
+            // 
+            // Number_2
+            // 
+            this.Number_2.AutoSize = true;
+            this.Number_2.Font = new System.Drawing.Font("Malgun Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Number_2.Location = new System.Drawing.Point(172, 199);
+            this.Number_2.Name = "Number_2";
+            this.Number_2.Size = new System.Drawing.Size(26, 25);
+            this.Number_2.TabIndex = 3;
+            this.Number_2.Text = "2.";
+            // 
+            // Number_1
+            // 
+            this.Number_1.AutoSize = true;
+            this.Number_1.Font = new System.Drawing.Font("Malgun Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Number_1.Location = new System.Drawing.Point(172, 139);
+            this.Number_1.Name = "Number_1";
+            this.Number_1.Size = new System.Drawing.Size(26, 25);
+            this.Number_1.TabIndex = 4;
+            this.Number_1.Text = "1.";
+            // 
+            // Number_3
+            // 
+            this.Number_3.AutoSize = true;
+            this.Number_3.Font = new System.Drawing.Font("Malgun Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Number_3.Location = new System.Drawing.Point(172, 278);
+            this.Number_3.Name = "Number_3";
+            this.Number_3.Size = new System.Drawing.Size(26, 25);
+            this.Number_3.TabIndex = 5;
+            this.Number_3.Text = "3.";
+            // 
+            // Price_Normal_Header
+            // 
+            this.Price_Normal_Header.AutoSize = true;
+            this.Price_Normal_Header.Font = new System.Drawing.Font("Malgun Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Price_Normal_Header.Location = new System.Drawing.Point(375, 100);
+            this.Price_Normal_Header.Name = "Price_Normal_Header";
+            this.Price_Normal_Header.Size = new System.Drawing.Size(51, 25);
+            this.Price_Normal_Header.TabIndex = 6;
+            this.Price_Normal_Header.Text = "Alm.";
+            // 
+            // Pizza_Description1
+            // 
+            this.Pizza_Description1.AutoSize = true;
+            this.Pizza_Description1.Font = new System.Drawing.Font("Malgun Gothic", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Pizza_Description1.Location = new System.Drawing.Point(224, 164);
+            this.Pizza_Description1.Name = "Pizza_Description1";
+            this.Pizza_Description1.Size = new System.Drawing.Size(133, 19);
+            this.Pizza_Description1.TabIndex = 7;
+            this.Pizza_Description1.Text = "Med tomat og ost";
+            // 
+            // Pizza_Description2
+            // 
+            this.Pizza_Description2.AutoSize = true;
+            this.Pizza_Description2.Font = new System.Drawing.Font("Malgun Gothic", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Pizza_Description2.Location = new System.Drawing.Point(224, 224);
+            this.Pizza_Description2.Name = "Pizza_Description2";
+            this.Pizza_Description2.Size = new System.Drawing.Size(154, 38);
+            this.Pizza_Description2.TabIndex = 8;
+            this.Pizza_Description2.Text = "Med tomat, ost, \r\nskinkekød og ananas";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Malgun Gothic", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(224, 303);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(119, 38);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Med tomat, ost \r\nog permaskinke";
+            // 
+            // Price_Family_Header
+            // 
+            this.Price_Family_Header.AutoSize = true;
+            this.Price_Family_Header.Font = new System.Drawing.Font("Malgun Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Price_Family_Header.Location = new System.Drawing.Point(457, 100);
+            this.Price_Family_Header.Name = "Price_Family_Header";
+            this.Price_Family_Header.Size = new System.Drawing.Size(52, 25);
+            this.Price_Family_Header.TabIndex = 10;
+            this.Price_Family_Header.Text = "Fam.";
+            // 
+            // Purchase1
+            // 
+            this.Purchase1.Location = new System.Drawing.Point(556, 139);
+            this.Purchase1.Name = "Purchase1";
+            this.Purchase1.Size = new System.Drawing.Size(69, 25);
+            this.Purchase1.TabIndex = 17;
+            this.Purchase1.Text = "Køb";
+            this.Purchase1.UseVisualStyleBackColor = true;
+            // 
+            // Purchase3
+            // 
+            this.Purchase3.Location = new System.Drawing.Point(556, 275);
+            this.Purchase3.Name = "Purchase3";
+            this.Purchase3.Size = new System.Drawing.Size(69, 25);
+            this.Purchase3.TabIndex = 18;
+            this.Purchase3.Text = "Køb";
+            this.Purchase3.UseVisualStyleBackColor = true;
+            // 
+            // Purchase2
+            // 
+            this.Purchase2.Location = new System.Drawing.Point(556, 199);
+            this.Purchase2.Name = "Purchase2";
+            this.Purchase2.Size = new System.Drawing.Size(69, 25);
+            this.Purchase2.TabIndex = 19;
+            this.Purchase2.Text = "Køb";
+            this.Purchase2.UseVisualStyleBackColor = true;
+            // 
+            // Price_Normal1
+            // 
+            this.Price_Normal1.AutoSize = true;
+            this.Price_Normal1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Price_Normal1.Location = new System.Drawing.Point(380, 139);
+            this.Price_Normal1.Name = "Price_Normal1";
+            this.Price_Normal1.Size = new System.Drawing.Size(56, 21);
+            this.Price_Normal1.TabIndex = 20;
+            this.Price_Normal1.Text = "75 ,-";
+            this.Price_Normal1.UseVisualStyleBackColor = true;
+            // 
+            // Price_Normal3
+            // 
+            this.Price_Normal3.AutoSize = true;
+            this.Price_Normal3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Price_Normal3.Location = new System.Drawing.Point(380, 278);
+            this.Price_Normal3.Name = "Price_Normal3";
+            this.Price_Normal3.Size = new System.Drawing.Size(56, 21);
+            this.Price_Normal3.TabIndex = 21;
+            this.Price_Normal3.Text = "75 ,-";
+            this.Price_Normal3.UseVisualStyleBackColor = true;
+            // 
+            // Price_Normal2
+            // 
+            this.Price_Normal2.AutoSize = true;
+            this.Price_Normal2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Price_Normal2.Location = new System.Drawing.Point(380, 199);
+            this.Price_Normal2.Name = "Price_Normal2";
+            this.Price_Normal2.Size = new System.Drawing.Size(56, 21);
+            this.Price_Normal2.TabIndex = 22;
+            this.Price_Normal2.Text = "75 ,-";
+            this.Price_Normal2.UseVisualStyleBackColor = true;
+            // 
+            // Price_Family3
+            // 
+            this.Price_Family3.AutoSize = true;
+            this.Price_Family3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Price_Family3.Location = new System.Drawing.Point(462, 278);
+            this.Price_Family3.Name = "Price_Family3";
+            this.Price_Family3.Size = new System.Drawing.Size(64, 21);
+            this.Price_Family3.TabIndex = 23;
+            this.Price_Family3.Text = "125 ,-";
+            this.Price_Family3.UseVisualStyleBackColor = true;
+            // 
+            // Price_Family2
+            // 
+            this.Price_Family2.AutoSize = true;
+            this.Price_Family2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Price_Family2.Location = new System.Drawing.Point(462, 199);
+            this.Price_Family2.Name = "Price_Family2";
+            this.Price_Family2.Size = new System.Drawing.Size(64, 21);
+            this.Price_Family2.TabIndex = 24;
+            this.Price_Family2.Text = "125 ,-";
+            this.Price_Family2.UseVisualStyleBackColor = true;
+            // 
+            // Price_Family1
+            // 
+            this.Price_Family1.AutoSize = true;
+            this.Price_Family1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Price_Family1.Location = new System.Drawing.Point(462, 139);
+            this.Price_Family1.Name = "Price_Family1";
+            this.Price_Family1.Size = new System.Drawing.Size(64, 21);
+            this.Price_Family1.TabIndex = 25;
+            this.Price_Family1.Text = "125 ,-";
+            this.Price_Family1.UseVisualStyleBackColor = true;
+            // 
+            // MenuKort
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1017, 559);
+            this.Controls.Add(this.Price_Family1);
+            this.Controls.Add(this.Price_Family2);
+            this.Controls.Add(this.Price_Family3);
+            this.Controls.Add(this.Price_Normal2);
+            this.Controls.Add(this.Price_Normal3);
+            this.Controls.Add(this.Price_Normal1);
+            this.Controls.Add(this.Purchase2);
+            this.Controls.Add(this.Purchase3);
+            this.Controls.Add(this.Purchase1);
+            this.Controls.Add(this.Price_Family_Header);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.Pizza_Description2);
+            this.Controls.Add(this.Pizza_Description1);
+            this.Controls.Add(this.Price_Normal_Header);
+            this.Controls.Add(this.Number_3);
+            this.Controls.Add(this.Number_1);
+            this.Controls.Add(this.Number_2);
+            this.Controls.Add(this.Pizza_Name2);
+            this.Controls.Add(this.Pizza_Name3);
+            this.Controls.Add(this.Pizza_Name1);
+            this.Name = "MenuKort";
+            this.Text = "MenuKort";
+            this.Load += new System.EventHandler(this.MenuKort_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Pizza_Name1;
+        private System.Windows.Forms.Label Pizza_Name3;
+        private System.Windows.Forms.Label Pizza_Name2;
+        private System.Windows.Forms.Label Number_2;
+        private System.Windows.Forms.Label Number_1;
+        private System.Windows.Forms.Label Number_3;
+        private System.Windows.Forms.Label Price_Normal_Header;
+        private System.Windows.Forms.Label Pizza_Description1;
+        private System.Windows.Forms.Label Pizza_Description2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label Price_Family_Header;
+        private System.Windows.Forms.Button Purchase1;
+        private System.Windows.Forms.Button Purchase3;
+        private System.Windows.Forms.Button Purchase2;
+        private System.Windows.Forms.CheckBox Price_Normal1;
+        private System.Windows.Forms.CheckBox Price_Normal3;
+        private System.Windows.Forms.CheckBox Price_Normal2;
+        private System.Windows.Forms.CheckBox Price_Family3;
+        private System.Windows.Forms.CheckBox Price_Family2;
+        private System.Windows.Forms.CheckBox Price_Family1;
+    }
+}
---
+++ Uge 14 - miniprojekt (Pizzeria)/MenuKort.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Uge_14___miniprojekt__Pizzeria_
+{
+    public partial class MenuKort : Form
+    {
+        public MenuKort()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuKort_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
