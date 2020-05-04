969765264a5ed7d9048a5766cc139e08d11231b9
KrusidOoO <KrusidOoO@users.noreply.github.com>
2020-05-04 10:57:21 +0200

    Tryna fix shiz

---
+++ SpaceInvaders/Space Invaders.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpaceInvaders
+{
+    public partial class Space_Invaders : Form
+    {
+        public Space_Invaders()
+        {
+            InitializeComponent();
+        }
+
+        private void Space_Invaders_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
